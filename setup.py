2737e0a63c7e02a96588470361ccab7c55260cd5
Bumblebee <git@bumblebee.cc>
2015-04-17 23:41:44 +0200

    Add a setup.py to be able to install it as a module

---
+++ setup.py
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='poppy_rate',
+    version='0.0.1',
+    author='Bumblebee',
+    author_email='git@bumblebee.cc',
+    py_modules=['poppy_rate'],
+    install_requires=['pypot', 'poppy_humanoid', 'zerorpc']
+)
