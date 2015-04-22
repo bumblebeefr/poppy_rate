fd90f6d670330a481347f27478e51742bd3b82fe
Bumblebee <git@bumblebee.cc>
2015-04-22 21:21:12 +0200

    Dependencies : back to main pypot repository, use svn bridge to install poppy-humanoid.

--- install.sh
+++ install.sh
@@ -4,10 +4,6 @@ cd `echo "$0" | sed -e "s/[^\/]*$//"`
 echo "Create virtualenv in local.virtualenv"
 virtualenv local.virtualenv
 
-mkdir local.lib
-
-echo "Clone poppy_humanoid to be able to install last version from git"
-git clone https://github.com/poppy-project/poppy-humanoid.git local.lib/poppy-humanoid
 
 echo "Install python dependencies"
 ./local.virtualenv/bin/pip install --pre -r requirements.txt
--- update.sh
+++ update.sh
@@ -1,11 +1,6 @@
 #! /bin/sh
 cd `echo "$0" | sed -e "s/[^\/]*$//"`
 
-echo "Pull poppy_hunmanoid last modifictions"
-cd local.lib/poppy-humanoid
-git pull
-cd -
-
 echo "Upgrading python dependencies"
 ./local.virtualenv/bin/pip install --pre -U --force-reinstall -r requirements.txt
 
