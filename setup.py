0668f03b3fc98ce5b367b516da579a071c8c15e3
bumblebee <git@bumblebee.cc>
2015-09-10 00:05:32 +0200

    cg version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='poppy-rate',
-    version='0.0.5',
+    version='0.0.6',
     packages=find_packages(),
     install_requires=['pypot', 'poppy_humanoid', 'poppy-creature'],
     include_package_data=True,
